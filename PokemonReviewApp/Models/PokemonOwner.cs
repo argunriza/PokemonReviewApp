a451a263f677afa0e5512483a86a94c4da87b62f
argunriza <argunrizaa@gmail.com>
2024-05-28 14:04:31 +0300

    Add project files.

---
+++ PokemonReviewApp/Models/PokemonOwner.cs
@@ -0,0 +1,15 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public  virtual Pokemon Pokemon { get; set; }
+
+        public virtual Owner Owner { get; set; }
+
+
+    }
+}
